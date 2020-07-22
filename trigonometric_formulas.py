2014ccc62b5d48eca607647fa0f61aefa80ac3da
Dheeraj <dheerajqm@gmail.com>
2020-07-22 23:02:51 +0530

    first commit

---
+++ trigonometric_formulas.py
@@ -0,0 +1,4 @@
+dict1 = {"sin2A" : "2sinAcosA" , "cos2A" : "cos^2A-sins^2A & 1-2sin^2A & 2cos^2A-1" , "tan2A" : "2tanA/1-tan^2A"}
+print("Search the Formula : ")
+data1 = input()
+print(data1, "=", dict1[data1])
